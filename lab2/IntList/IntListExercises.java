a9dbb552b4e8e3a99a37acd88635d8407f07cf92
10300749 <wang.haode@zte.com.cn>
2022-11-04 15:15:09 +0800

    CS61B SP21 lab2 fix bugs in addConstant() and sum()

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -9,6 +9,9 @@ public class IntListExercises {
      * @param lst IntList from Lecture
      */
     public static void addConstant(IntList lst, int c) {
+        if (lst == null) {
+            return;
+        }
         IntList head = lst;
         while (head.rest != null) {
             head.first += c;
