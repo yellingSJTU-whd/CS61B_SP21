b65788299cf2e17349f7286bb5ba1ad3c6b7dcb7
10300749 <wang.haode@zte.com.cn>
2022-11-07 17:20:05 +0800

    CS61B SP21 project1 ArrayDeque helper methods

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,18 +1,59 @@
 package deque;
 
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Iterator;
+import java.util.Objects;
 
-public class ArrayDeque<T> implements Deque<T>{
+public class ArrayDeque<T> implements Deque<T> {
+
+    private T[] items;
+    private int head;
+    private int tail;
+
+
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        head = 0;
+        tail = 0;
+    }
+
     @Override
     public void addFirst(@NotNull T item) {
-
+        Objects.requireNonNull(item);
     }
 
     @Override
     public void addLast(@NotNull T item) {
 
+    }
+
+    /**
+     * Circularly increase i by 1, mod modulus.
+     */
+    @Contract(pure = true)
+    private static int inc(int i, int modulus) {
+        if (++i >= modulus) i = 0;
+        return i;
+    }
+
+    /**
+     * Circularly decrease i by 1, mod modulus.
+     */
+    @Contract(pure = true)
+    private static int dec(int i, int modulus) {
+        if (--i < 0) i = modulus - 1;
+        return i;
+    }
+
+    /**
+     * Subtracts j from i, mod modulus.
+     */
+    @Contract(pure = true)
+    private static int sub(int i, int j, int modulus) {
+        if ((i -= j) < 0) i += modulus;
+        return i;
     }
 
     @Override
